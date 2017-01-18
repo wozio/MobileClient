dadfa74f59525c7de02452f117c439f6d80861f1
wozio <woziosek@gmail.com>
2017-01-18 22:18:01 +0100

    .

---
+++ MobileClient.Core/Services/Discovery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sockets.Plugin;
+
+namespace MobileClient.Core.Services
+{
+  class Discovery
+  {
+    UdpSocketMulticastClient receiver_;
+    Discovery()
+    {
+      
+      receiver_ = new UdpSocketMulticastClient();
+      receiver_.TTL = 5;
+
+      receiver_.MessageReceived += (sender, args) =>
+      {
+        var from = String.Format("{0}:{1}", args.RemoteAddress, args.RemotePort);
+        var data = Encoding.UTF8.GetString(args.ByteData, 0, args.ByteData.Length);
+
+        //Debug.WriteLine("{0} - {1}", from, data);
+      };
+      join();
+    }
+    private async void join()
+    {
+      var port = 10001;
+      var address = "239.255.255.255";
+      await receiver_.JoinMulticastGroupAsync(address, port);
+    }
+  }
+}
---
+++ MobileClient.Core/Services/IDiscovery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileClient.Core.Services
+{
+  public interface IDiscovery
+  {
+    event EventHandler ServiceAvailability;
+  }
+
+  public class ServiceAvailabilityData : EventArgs
+  {
+
+  }
+}
